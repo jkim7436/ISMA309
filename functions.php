b01bf1feb51ec25ee85fd11cf2bec9ef60c2a719
Jeein Kim <jkim7436@gmail.com>
2014-11-18 14:17:27 -0800

    some changes

--- functions.php
+++ functions.php
@@ -34,8 +34,8 @@ add_action( 'pre_get_posts', 'my_add_reviews' );
 // Equeue Isotope and isotope settings
 function reviews_scripts() {
     if(is_archive('review')){
-        wp_enqueue_script( 'isotope-lib', get_stylesheet_directory_uri() . '/js/isotope.min.js', array('jquery'), 11112014, false );
-        wp_enqueue_script( 'isotope-settings', get_stylesheet_directory_uri() . '/js/isotope.settings.js', array('isotope-lib'), 11112014, false );
+        wp_enqueue_script( 'isotope-lib', get_stylesheet_directory_uri() . '../js/isotope.min.js', array('jquery'), 11112014, false );
+        wp_enqueue_script( 'isotope-settings', get_stylesheet_directory_uri() . '../js/isotope.settings.js', array('isotope-lib'), 11112014, false );
     }
 }   
 
